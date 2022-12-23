ad14c848b21ecd1455d007ea092d03af0638f040
Suneetha Konda <116636454+suneethakonda@users.noreply.github.com>
2022-12-23 15:50:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,21 +2,6 @@
 import streamlit
 #import pandas
 #import requests
-streamlit.title('My Parents New Healthy Diner') 
-
-streamlit.header('BF Menu')
-streamlit.title('oats')
-streamlit.title('Smoothie')
-streamlit.title('Egg')
-streamlit.header('Build your own smoothie')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
-streamlit.stop()
 import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
